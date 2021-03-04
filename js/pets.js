d2b922b2aab93f13e877e146fa60bf181db9203a
spsteady <spsteady@gmail.com>
2021-03-04 16:44:22 -0600

    Initial Upload

---
+++ js/pets.js
@@ -0,0 +1,48 @@
+/********************************************************/
+/*  Array of Objects - (Only change code in this file   */    
+/********************************************************/
+
+const pets = [
+  {
+    name: 'Billy',
+    type: 'Dog',
+    breed: 'Austrailian Shepherd',
+    age: 2,
+    photo: 'img/aussie.jpg'
+  },
+  {
+    name: 'Donnie',
+    type: 'Dog',
+    breed: 'Dachshund',
+    age: 5,
+    photo: 'img/dachshund.jpg'
+  },
+  {
+    name: 'Marie',
+    type: 'Dog',
+    breed: 'Golden Retriever',
+    age: 1,
+    photo: 'img/golden.jpg'
+  },
+  {
+    name: 'Layla',
+    type: 'Cat',
+    breed: 'Persian Minx',
+    age: 6,
+    photo: 'img/persian.jpg'
+  },
+  {
+    name: 'Jacko',
+    type: 'Dog',
+    breed: 'Pug',
+    age: 3,
+    photo: 'img/pug.jpg'
+  },
+  {
+    name: 'Cheech',
+    type: 'Cat',
+    breed: 'Tabby',
+    age: 1,
+    photo: 'img/tabby.jpg'
+  }
+];
